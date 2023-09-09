fafbd46897e718a30146cdf12b046758827d1d60
rexton-george <rextongeorgecseb@gmail.com>
2023-09-09 18:36:25 +0530

    added

---
+++ toggle/toggle.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById('darkMode');
+const body = document.querySelector('body');
+
+
+toggle.addEventListener('click',function(){
+    this.classList.toggle('bi-moon-fill');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background='white';
+        body.style.color='black'
+        body.style.transition='0.6s'
+    }
+    else{
+        body.style.background='black';
+        body.style.color='white';
+    }
+})
